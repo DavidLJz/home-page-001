778a0e848ee81c3250039947479f03f7f46a8e9b
David LJ <4dlj1995@gmail.com>
2021-10-24 23:22:19 -0500

    cambios

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 import { docReady } from "./modules/doc-ready.js";
 
 docReady(function () {
+  const terminal = document.getElementById('terminal');
   const cmd = document.querySelectorAll('.cmd');
 
-  document.body.addEventListener('click', function () {
-    console.log(this.classList)
+  document.body.addEventListener('change', function (e) {
+    const el = e.target;
+
+    if ( el.classList.contains('cmd') ) {
+      const log = document.createElement('div');
+      log.classList = 'command-log';
+      log.textContent = el.value;
+
+      terminal.querySelector('.command-logs-container').append( log );
+    }
   });
 });
