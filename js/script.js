72c935d598dfa74fa847757c92677494afd4a593
David LJ <4dlj1995@gmail.com>
2021-10-30 12:25:04 -0500

    changed class .command-log > command-line

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import { docReady } from "./modules/doc-ready.js";
 
 docReady(function () {
   const terminal = document.getElementById('terminal');
-  const terminal_output = terminal.querySelector('.command-logs-container');
+  const terminal_output = terminal.querySelector('.command-lines-container');
 
   terminal.querySelector('.input-container').onsubmit = function (e) {
     e.preventDefault();
@@ -16,6 +16,24 @@ docReady(function () {
       return;
     }
 
-    terminal_output.innerHTML += `<div class="command-log">${value}</div>`;
+    let line = document.createElement('div');
+    line.classList.add('command-line');
+
+    switch (value) {
+      case 'help':
+        line.textContent = 'Available commands: clear, help, about';
+        break;
+
+      case 'about':
+        line.textContent = 'Made by DavidLjz';
+        break;
+
+      default: {
+        line.textContent = 'Unknown command';
+        break;
+      }
+    }
+
+    terminal_output.appendChild(line);
   };
 });
