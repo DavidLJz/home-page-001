a574911b187d3797025a02a7c5ad8de7057a2bb0
David LJ <4dlj1995@gmail.com>
2021-10-30 12:20:07 -0500

    added clear command

--- js/script.js
+++ js/script.js
@@ -2,14 +2,20 @@ import { docReady } from "./modules/doc-ready.js";
 
 docReady(function () {
   const terminal = document.getElementById('terminal');
-  const cmd = document.querySelectorAll('.cmd');
   const terminal_output = terminal.querySelector('.command-logs-container');
 
   terminal.querySelector('.input-container').onsubmit = function (e) {
     e.preventDefault();
     const input = this.querySelector('input');
     const value = input.value;
+
     input.value = '';
+
+    if (value === 'clear') {
+      terminal_output.innerHTML = '';
+      return;
+    }
+
     terminal_output.innerHTML += `<div class="command-log">${value}</div>`;
   };
 });
