d9863c5d99c7935ab75581ebcf861d32b0a429b6
David LJ <4dlj1995@gmail.com>
2021-10-31 16:43:38 -0600

    add command go to url

--- js/script.js
+++ js/script.js
@@ -16,6 +16,12 @@ docReady(function () {
       return;
     }
 
+    let command_input = document.createElement('div');
+    command_input.classList.add('command-line');
+    command_input.textContent = value;
+
+    terminal_output.append(command_input);
+
     let line = document.createElement('div');
     line.classList.add('command-line');
 
@@ -55,7 +61,16 @@ docReady(function () {
   };
 
   const goCommand = (line) => {
-    const destination = line.split(' ')[1];
+    line = line.split(' ');
+
+    let usage = 'Usage: go [destination] [args]\n\n' + 
+      '\tExamples:\n' + 
+      '\t- go home\n' + 
+      '\t- go back\n' + 
+      '\t- go url google.com';
+
+    const destination = line[1];
+    let args = line.slice(2);
 
     switch (destination) {
       case 'home': {
@@ -67,9 +82,66 @@ docReady(function () {
         history.back();
         break;
       }
+
+      case 'url': {
+        if ( !args.length || !args[0] ) {
+          throw new Error('Wrong syntax.\n\n' + usage);
+        }
+
+        let url = args[0];
+        args = args.slice(1);
+
+        const params = {
+          'ssl' : true,
+          'timeout' : 1000
+        };
+
+        for (const arg of args) {
+          switch (arg) {
+            case '--no-ssl':{
+              params.ssl = false;
+              break;
+            }
+
+            case '--timeout':
+            case '-t': {
+              params.timeout = parseInt(args[args.indexOf(arg) + 1]);
+              break;
+            }
+
+            default: break;
+          }
+        }
+
+        if ( !url.match(/^https?:\/\//) ) {
+          if ( params.ssl ) {
+            url = 'https://' + url;
+          }
+
+          else {
+            url = 'http://' + url;
+          }
+        }
+
+        const line = document.createElement('p');
+        line.classList = 'command-line';
+        line.innerHTML = `Redirecting to ${url} in <span>${params.timeout / 1000}</span>s. If not `;
+        line.innerHTML += '<a href="' + url + '">click here</a>.';
+
+        terminal_output.append(line);
+
+        setInterval(() => {
+          line.querySelector('span').textContent = parseInt(line.querySelector('span').textContent) - 1;
+        }, 1000);
+
+        setTimeout(() => { location.assign(url); }, params.timeout);
+        
+        console.log({url, params, url, args});
+        break;
+      }
     
       default: {
-        throw new Error('Unknown destination');
+        throw new Error('Unknown destination\n\n' + usage);
       }
     }
   } 
